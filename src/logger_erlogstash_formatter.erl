661616925faa4fd161d0e76ab5f70ada154b1082
Led <ledest@gmail.com>
2023-01-03 19:14:02 +0200

    logger_erlogstash_formatter: add support timestamp formats: iso8601, unix_ms, unix
    - update encode/2
    - meta/3
    - add timestamp/2

--- src/logger_erlogstash_formatter.erl
+++ src/logger_erlogstash_formatter.erl
@@ -4,8 +4,10 @@
 -export([check_config/1, format/2]).
 
 -define(DEFAULT_FORMAT, json).
+-define(DEFAULT_TIMESTAMP, iso8601).
 
 -type format() :: json | json_line | msgpack.
+-type timestamp_format() :: iso8601 | unix_ms | unix.
 -type value() :: null | boolean() | atom() | binary() | number() | [value()] | #{atom() => value()}.
 -type data() :: #{atom() => value()}.
 -type msg() :: {io:format(), [term()]} | {report, logger:report()} | {string, unicode:chardata()}.
@@ -15,10 +17,13 @@ check_config(#{format := F}) when F =/= json, F =/= json_line, F =/= msgpack -> 
 check_config(_) -> ok.
 
 -spec format(LogEvent::logger:log_event(), Config::logger_formatter:config()) -> unicode:chardata().
-format(#{level := Level, msg := Msg, meta := Meta}, Config) ->
-    encode(maps:get(format, Config, ?DEFAULT_FORMAT),
-           (meta(Meta))#{level => Level, node => node(), type => erlogstash,
-                         message => unicode:characters_to_binary(msg(Msg, Meta))}).
+format(#{level := Level, msg := Msg, meta := #{time := T} = Meta}, Config) ->
+    encode((meta(Meta))#{level => Level,
+                         type => erlogstash,
+                         '@timestamp' => timestamp(T, maps:get(timestamp, Config, ?DEFAULT_TIMESTAMP)),
+                         node => node(),
+                         message => unicode:characters_to_binary(msg(Msg, Meta))},
+           maps:get(format, Config, ?DEFAULT_FORMAT)).
 
 %% internal functions
 
@@ -35,11 +40,9 @@ msg({F, A}, _) -> io_lib:format(F, A).
 meta(Meta) -> maps:fold(fun meta/3, #{}, Meta).
 
 -spec meta(Tag::atom(), V::term(), A::data()) -> data().
-meta(time, V, A) ->
-    A#{'@timestamp' => list_to_binary(calendar:system_time_to_rfc3339(V, [{unit, microsecond}, {offset, "Z"}]))};
 meta(file, V, A) -> A#{file => unicode:characters_to_binary(V)};
 meta(mfa, {M, F, Arity}, A) -> A#{module => M, function => F, arity => Arity, mfa => mfa(M, F, Arity)};
-meta(K, _, A) when K =:= error_logger; K =:= logger_formatter; K =:= report_cb; K =:= gl -> A;
+meta(K, _, A) when K =:= time; K =:= error_logger; K =:= logger_formatter; K =:= report_cb; K =:= gl -> A;
 meta(K, V, A) -> A#{K => value(V)}.
 
 -spec value(term()) -> value().
@@ -70,7 +73,14 @@ value(F) when is_function(F) ->
 mfa(M, F, A) ->
     <<(atom_to_binary(M, latin1))/binary, $:, (atom_to_binary(F, utf8))/binary, $/, (integer_to_binary(A))/binary>>.
 
--spec encode(Format::format(), Data::data()) -> iodata().
-encode(json, Data) -> jsone:encode(Data);
-encode(json_line, Data) -> [encode(json, Data), $\n];
-encode(msgpack, Data) -> msgpack:pack(Data, [{pack_str, none}]).
+-spec timestamp(T::pos_integer(), Format::timestamp_format()) -> binary() | pos_integer().
+timestamp(T, unix_ms) -> erlang:convert_time_unit(T, microsecond, millisecond);
+timestamp(T, unix) -> erlang:convert_time_unit(T, microsecond, second);
+timestamp(T, iso8601) ->
+    list_to_binary(calendar:system_time_to_rfc3339(timestamp(T, unix_ms), [{unit, millisecond}, {offset, "Z"}]));
+timestamp(T, _) -> T.
+
+-spec encode(Data::data(), Format::format()) -> iodata().
+encode(Data, json) -> jsone:encode(Data);
+encode(Data, json_line) -> [encode(json, Data), $\n];
+encode(Data, msgpack) -> msgpack:pack(Data, [{pack_str, none}]).
