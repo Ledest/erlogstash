0185e93f3e8878b129157d7bb610a4961af5d313
Led <ledest@gmail.com>
2023-03-19 15:55:58 +0200

    logger_erlogstash_h: add changing_config/3

--- src/logger_erlogstash_h.erl
+++ src/logger_erlogstash_h.erl
@@ -1,7 +1,7 @@
 -module(logger_erlogstash_h).
 
 %% logger callbacks
--export([adding_handler/1, removing_handler/1, log/2]).
+-export([adding_handler/1, removing_handler/1, log/2, changing_config/3]).
 
 -define(VALID_CONFIG_KEYS, [format, timestamp, tags]).
 
@@ -17,6 +17,20 @@ removing_handler(#{id := N}) -> erlogstash:stop_worker(N).
 
 -spec log(LogEvent::logger:log_event(), HConfig::logger:handler_config()) -> ok.
 log(LogEvent, #{id := N, formatter := {M, FC}}) -> erlogstash:send(N, M:format(LogEvent, FC)).
+
+-spec changing_config(set|update, OConfig::logger:handler_config(), NConfig::logger:handler_config()) ->
+          {ok, logger:handler_config()} | {error, term()}.
+changing_config(set, #{id := N, output := Output}, #{id := N, output := Output} = NConfig) -> {ok, config(NConfig)};
+changing_config(set, #{id := N}, #{id := N} = NConfig) ->
+    erlogstash:stop_worker(N),
+    adding_handler(NConfig);
+changing_config(set, _, NConfig) -> adding_handler(NConfig);
+changing_config(update, #{id := N, output := Output} = OConfig, #{id := N, output := Output} = NConfig) ->
+    {ok, config(maps:merge(OConfig, NConfig))};
+changing_config(update, #{id := N} = OConfig, #{id := N} = NConfig) ->
+    erlogstash:stop_worker(N),
+    adding_handler(maps:merge(OConfig, NConfig));
+changing_config(set, OConfig, NConfig) -> adding_handler(maps:merge(OConfig, NConfig)).
 
 %% internal functions
 -spec config(HConfig::logger:handler_config()) -> logger:handler_config().
